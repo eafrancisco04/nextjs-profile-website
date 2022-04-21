57b0ace3921815093a7254d9541e05fbc120a11c
Ea Francisco <emberkasai04@gmail.com>
2022-04-21 17:22:36 -0400

    finish portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -4,6 +4,8 @@ import Head from 'next/head';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css';
 import sample1 from '../public/images/music-player.png';
+import sample2 from '../public/images/kanji-app.png';
+import sample3 from '../public/images/gallery-app.png';
 
 
 export default function FirstPost() {
@@ -25,12 +27,36 @@ export default function FirstPost() {
             <h3>Music Player</h3>
             <Image
               src={sample1}
-              alt="Music Player Perview"
-              height={400}
+              alt="Music Player Preview"
+              height={350}
               width={600} />
-            <p>A simple music player app made with vanilla Javascript</p>
+            <p>A simple music player app made with vanilla Javascript. Features include playing and pause music, repeating songback to start, adjusting volume, and search.</p>
             <button className={styles.button}>
-              <Link href="/posts/first-post">Live Demo</Link></button>
+              <Link href="https://eafrancisco04.github.io/music-player-project/">Live Demo</Link></button>
+        </div>
+
+        <div className={styles.card}>
+            <h3>Kanji App</h3>
+            <Image
+              src={sample2}
+              alt="Kanji App"
+              height={300}
+              width={600} />
+            <p>A random Kanji generating website made using Javascript. The app gets the info from an API and prints out the character, writing video, and meanings.</p>
+            <button className={styles.button}>
+              <Link href="https://eafrancisco04.github.io/kanji-api/">Live Demo</Link></button>
+        </div>
+
+        <div className={styles.card}>
+            <h3>Gallery App</h3>
+            <Image
+              src={sample3}
+              alt="Image Gallery"
+              height={300}
+              width={600} />
+            <p>Using firebase, our group created an image gallery that includes a login feature, image upload and download, delete image, and features for editing your profile and password.</p>
+            <button className={styles.button}>
+              <Link href="https://wddm120-a2.github.io/PhotoGalleryApp/gallery.html">Live Demo</Link></button>
         </div>
 
       </div>
